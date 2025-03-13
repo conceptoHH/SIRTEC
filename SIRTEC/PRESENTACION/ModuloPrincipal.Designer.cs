6ea840f1db1c86b3a7ea4e1173d3a9efbb31906a
conceptoHH <syahirmt3@gmail.com>
2025-03-13 07:38:23 -0600

    gg

--- SIRTEC/PRESENTACION/ModuloPrincipal.Designer.cs
+++ SIRTEC/PRESENTACION/ModuloPrincipal.Designer.cs
@@ -33,20 +33,15 @@
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.btnInsc = new System.Windows.Forms.Button();
-            this.panel7 = new System.Windows.Forms.Panel();
-            this.btnCoord = new System.Windows.Forms.Button();
-            this.panel6 = new System.Windows.Forms.Panel();
-            this.btnDocentes = new System.Windows.Forms.Button();
-            this.panel5 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
+            this.pnlPadre = new System.Windows.Forms.Panel();
             this.btnReinsc = new System.Windows.Forms.Button();
-            this.panel8 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDocentes = new System.Windows.Forms.Button();
+            this.btnInsc = new System.Windows.Forms.Button();
+            this.btnCoord = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.panel3.SuspendLayout();
+            this.pnlPadre.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -56,9 +51,8 @@
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1579, 171);
+            this.panel1.Size = new System.Drawing.Size(1184, 139);
             this.panel1.TabIndex = 0;
             // 
             // label1
@@ -67,10 +61,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.Font = new System.Drawing.Font("Bahnschrift Condensed", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(327, 30);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(245, 24);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(1017, 80);
+            this.label1.Size = new System.Drawing.Size(763, 65);
             this.label1.TabIndex = 2;
             this.label1.Text = "Técnologico Nacional de México";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -81,9 +74,8 @@
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(214, 171);
+            this.pictureBox1.Size = new System.Drawing.Size(160, 139);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
@@ -91,166 +83,113 @@
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(80)))), ((int)(((byte)(157)))));
             this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 1027);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel2.Location = new System.Drawing.Point(0, 834);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1579, 33);
+            this.panel2.Size = new System.Drawing.Size(1184, 27);
             this.panel2.TabIndex = 1;
+            // 
+            // pnlPadre
+            // 
+            this.pnlPadre.Controls.Add(this.btnReinsc);
+            this.pnlPadre.Controls.Add(this.label2);
+            this.pnlPadre.Controls.Add(this.btnDocentes);
+            this.pnlPadre.Controls.Add(this.btnInsc);
+            this.pnlPadre.Controls.Add(this.btnCoord);
+            this.pnlPadre.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlPadre.Location = new System.Drawing.Point(0, 139);
+            this.pnlPadre.Name = "pnlPadre";
+            this.pnlPadre.Size = new System.Drawing.Size(1184, 695);
+            this.pnlPadre.TabIndex = 4;
+            // 
+            // btnReinsc
+            // 
+            this.btnReinsc.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnReinsc.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnReinsc.FlatAppearance.BorderSize = 0;
+            this.btnReinsc.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnReinsc.Font = new System.Drawing.Font("Bahnschrift Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReinsc.Location = new System.Drawing.Point(417, 532);
+            this.btnReinsc.Name = "btnReinsc";
+            this.btnReinsc.Size = new System.Drawing.Size(375, 91);
+            this.btnReinsc.TabIndex = 8;
+            this.btnReinsc.Text = "Reinscripción";
+            this.btnReinsc.UseVisualStyleBackColor = true;
             // 
             // label2
             // 
             this.label2.Dock = System.Windows.Forms.DockStyle.Top;
             this.label2.Font = new System.Drawing.Font("Bahnschrift Condensed", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(0, 171);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(0, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(1579, 107);
-            this.label2.TabIndex = 3;
+            this.label2.Size = new System.Drawing.Size(1184, 87);
+            this.label2.TabIndex = 0;
             this.label2.Text = "Bienvenido. Elige tu opcion:";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // panel3
-            // 
-            this.panel3.Controls.Add(this.btnReinsc);
-            this.panel3.Controls.Add(this.panel8);
-            this.panel3.Controls.Add(this.btnInsc);
-            this.panel3.Controls.Add(this.panel7);
-            this.panel3.Controls.Add(this.btnCoord);
-            this.panel3.Controls.Add(this.panel6);
-            this.panel3.Controls.Add(this.btnDocentes);
-            this.panel3.Controls.Add(this.panel5);
-            this.panel3.Controls.Add(this.panel4);
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel3.Location = new System.Drawing.Point(0, 278);
-            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1579, 749);
-            this.panel3.TabIndex = 4;
-            // 
-            // btnInsc
-            // 
-            this.btnInsc.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnInsc.FlatAppearance.BorderColor = System.Drawing.Color.White;
-            this.btnInsc.FlatAppearance.BorderSize = 0;
-            this.btnInsc.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.btnInsc.Font = new System.Drawing.Font("Bahnschrift Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnInsc.Location = new System.Drawing.Point(649, 332);
-            this.btnInsc.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.btnInsc.Name = "btnInsc";
-            this.btnInsc.Size = new System.Drawing.Size(258, 112);
-            this.btnInsc.TabIndex = 6;
-            this.btnInsc.Text = "Inscripciones";
-            this.btnInsc.UseVisualStyleBackColor = true;
-            // 
-            // panel7
-            // 
-            this.panel7.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel7.Location = new System.Drawing.Point(649, 278);
-            this.panel7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.panel7.Name = "panel7";
-            this.panel7.Size = new System.Drawing.Size(258, 54);
-            this.panel7.TabIndex = 5;
-            // 
-            // btnCoord
-            // 
-            this.btnCoord.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnCoord.FlatAppearance.BorderColor = System.Drawing.Color.White;
-            this.btnCoord.FlatAppearance.BorderSize = 0;
-            this.btnCoord.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.btnCoord.Font = new System.Drawing.Font("Bahnschrift Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCoord.Location = new System.Drawing.Point(649, 166);
-            this.btnCoord.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.btnCoord.Name = "btnCoord";
-            this.btnCoord.Size = new System.Drawing.Size(258, 112);
-            this.btnCoord.TabIndex = 4;
-            this.btnCoord.Text = "Coordinador";
-            this.btnCoord.UseVisualStyleBackColor = true;
-            // 
-            // panel6
-            // 
-            this.panel6.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel6.Location = new System.Drawing.Point(649, 112);
-            this.panel6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.panel6.Name = "panel6";
-            this.panel6.Size = new System.Drawing.Size(258, 54);
-            this.panel6.TabIndex = 3;
-            // 
             // btnDocentes
             // 
-            this.btnDocentes.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDocentes.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDocentes.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.btnDocentes.FlatAppearance.BorderColor = System.Drawing.Color.White;
             this.btnDocentes.FlatAppearance.BorderSize = 0;
             this.btnDocentes.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.btnDocentes.Font = new System.Drawing.Font("Bahnschrift Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDocentes.Location = new System.Drawing.Point(649, 0);
-            this.btnDocentes.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnDocentes.Location = new System.Drawing.Point(417, 133);
             this.btnDocentes.Name = "btnDocentes";
-            this.btnDocentes.Size = new System.Drawing.Size(258, 112);
+            this.btnDocentes.Size = new System.Drawing.Size(375, 91);
             this.btnDocentes.TabIndex = 0;
             this.btnDocentes.Text = "Docentes";
             this.btnDocentes.UseVisualStyleBackColor = true;
             this.btnDocentes.Click += new System.EventHandler(this.button1_Click);
             // 
-            // panel5
+            // btnInsc
             // 
-            this.panel5.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel5.Location = new System.Drawing.Point(907, 0);
-            this.panel5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(672, 749);
-            this.panel5.TabIndex = 2;
+            this.btnInsc.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnInsc.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnInsc.FlatAppearance.BorderSize = 0;
+            this.btnInsc.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnInsc.Font = new System.Drawing.Font("Bahnschrift Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsc.Location = new System.Drawing.Point(417, 400);
+            this.btnInsc.Name = "btnInsc";
+            this.btnInsc.Size = new System.Drawing.Size(375, 91);
+            this.btnInsc.TabIndex = 6;
+            this.btnInsc.Text = "Inscripciones";
+            this.btnInsc.UseVisualStyleBackColor = true;
             // 
-            // panel4
+            // btnCoord
             // 
-            this.panel4.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel4.Location = new System.Drawing.Point(0, 0);
-            this.panel4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(649, 749);
-            this.panel4.TabIndex = 1;
-            // 
-            // btnReinsc
-            // 
-            this.btnReinsc.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnReinsc.FlatAppearance.BorderColor = System.Drawing.Color.White;
-            this.btnReinsc.FlatAppearance.BorderSize = 0;
-            this.btnReinsc.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.btnReinsc.Font = new System.Drawing.Font("Bahnschrift Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnReinsc.Location = new System.Drawing.Point(649, 498);
-            this.btnReinsc.Margin = new System.Windows.Forms.Padding(4);
-            this.btnReinsc.Name = "btnReinsc";
-            this.btnReinsc.Size = new System.Drawing.Size(258, 112);
-            this.btnReinsc.TabIndex = 8;
-            this.btnReinsc.Text = "Reinscripción";
-            this.btnReinsc.UseVisualStyleBackColor = true;
-            // 
-            // panel8
-            // 
-            this.panel8.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel8.Location = new System.Drawing.Point(649, 444);
-            this.panel8.Margin = new System.Windows.Forms.Padding(4);
-            this.panel8.Name = "panel8";
-            this.panel8.Size = new System.Drawing.Size(258, 54);
-            this.panel8.TabIndex = 7;
+            this.btnCoord.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCoord.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnCoord.FlatAppearance.BorderSize = 0;
+            this.btnCoord.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnCoord.Font = new System.Drawing.Font("Bahnschrift Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCoord.Location = new System.Drawing.Point(417, 265);
+            this.btnCoord.Name = "btnCoord";
+            this.btnCoord.Size = new System.Drawing.Size(375, 91);
+            this.btnCoord.TabIndex = 4;
+            this.btnCoord.Text = "Coordinador";
+            this.btnCoord.UseVisualStyleBackColor = true;
             // 
             // ModuloPrincipal
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
-            this.ClientSize = new System.Drawing.Size(1579, 1060);
-            this.Controls.Add(this.panel3);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(1184, 861);
+            this.Controls.Add(this.pnlPadre);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ModuloPrincipal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ModuloPrincipal";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.panel3.ResumeLayout(false);
+            this.pnlPadre.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -261,16 +200,11 @@
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Panel panel5;
-        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel pnlPadre;
+        private System.Windows.Forms.Button btnReinsc;
         private System.Windows.Forms.Button btnDocentes;
         private System.Windows.Forms.Button btnInsc;
-        private System.Windows.Forms.Panel panel7;
         private System.Windows.Forms.Button btnCoord;
-        private System.Windows.Forms.Panel panel6;
-        private System.Windows.Forms.Button btnReinsc;
-        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Label label2;
     }
 }
--- SIRTEC/PRESENTACION/ctInscripcion.Designer.cs
+++ SIRTEC/PRESENTACION/ctInscripcion.Designer.cs
@@ -28,10 +28,180 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dgvDocumentos = new System.Windows.Forms.DataGridView();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDocumentos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Bahnschrift SemiLight", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(51, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre(s):";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Bahnschrift", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(51, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(677, 23);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Completa el sigiente formulario para realizar tu proceso de reinscripcion";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(254, 121);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(206, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Bahnschrift SemiLight", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(51, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 23);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Apellido Paterno:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(254, 164);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(206, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Bahnschrift SemiLight", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(51, 202);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(162, 23);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Apellido Materno:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(254, 207);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(206, 20);
+            this.textBox3.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Bahnschrift SemiLight", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(51, 245);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(197, 23);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Fecha de Nacimiento:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(254, 248);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(206, 20);
+            this.dateTimePicker1.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Bahnschrift SemiLight", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(51, 284);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(197, 23);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "E-mail:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(254, 287);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(206, 20);
+            this.textBox4.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dgvDocumentos);
+            this.panel1.Location = new System.Drawing.Point(666, 164);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(562, 524);
+            this.panel1.TabIndex = 3;
+            // 
+            // dgvDocumentos
+            // 
+            this.dgvDocumentos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDocumentos.Location = new System.Drawing.Point(3, 14);
+            this.dgvDocumentos.Name = "dgvDocumentos";
+            this.dgvDocumentos.Size = new System.Drawing.Size(537, 30);
+            this.dgvDocumentos.TabIndex = 0;
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Bahnschrift SemiLight", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(662, 116);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(212, 23);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Documentos subidos:";
+            // 
+            // ctInscripcion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "ctInscripcion";
+            this.Size = new System.Drawing.Size(1251, 711);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDocumentos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DataGridView dgvDocumentos;
     }
 }
